a9d92914d35d7a4692206b9483ec2c020de44235
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-01-07 15:27:58 -0500

    moved current weather destructuring to weather class

--- assets/02-scripts/lib/weather.js
+++ assets/02-scripts/lib/weather.js
@@ -20,8 +20,28 @@ class Weather extends API {
 		this.url.pathname += "/weather";
 		const response = await this.getWeather();
 		this.resetURL();
-		// const weather {} = response;
-		return response;
+		const weather = {
+			dateTime: luxon.DateTime.fromSeconds(response.dt),
+			icon: response.weather.icon,
+			weatherId: response.weather.id,
+			temperature: response.main.temp,
+			feelsLikeTemp: response.main.feels_like,
+			lowTemp: response.main.temp_min,
+			highTemp: response.main.temp_max,
+			pressure: response.main.pressure / 33.863886666667, // measured in hPa (100x Pa). Converted from hPa to inHg
+			humidity: response.main.humidity / 100, // Transformed to percent
+			visibility: response.visibility, // max is 10,000 meters
+			windSpeed: response.wind.speed, // miles/hr
+			windDirection: response.wind.deg,
+			windGust: response.wind.gust, // miles/hr
+			cloudiness: response.clouds.all / 100, // Transformed to percent
+			rainVolume: response.rain,
+			snowVolume: response.snow,
+			sunrise: luxon.DateTime.fromSeconds(response.sys.sunrise),
+			sunset: luxon.DateTime.fromSeconds(response.sys.sunset),
+			weather: response.weather, // Full weather output if needed
+		};
+		return weather;
 	}
 
 	async getExtendedForecast(days, latitude, longitude) {
--- assets/02-scripts/script.js
+++ assets/02-scripts/script.js
@@ -20,30 +20,7 @@ function addToSearchHistory(latitude, longitude, location) {
 }
 
 async function fetchCurrentWeather(latitude, longitude) {
-	let response;
-	response = await weatherAPI.getCurrentWeather(latitude, longitude);
-	const weather = {
-		dateTime: luxon.DateTime.fromSeconds(response.dt),
-		icon: response.weather.icon,
-		weatherId: response.weather.id,
-		temperature: response.main.temp,
-		feelsLikeTemp: response.main.feels_like,
-		lowTemp: response.main.temp_min,
-		highTemp: response.main.temp_max,
-		pressure: response.main.pressure / 33.863886666667, // measured in hPa (100x Pa). Converted from hPa to inHg
-		humidity: response.main.humidity / 100, // Transformed to percent
-		visibility: response.visibility, // max is 10,000 meters
-		windSpeed: response.wind.speed, // miles/hr
-		windDirection: response.wind.deg,
-		windGust: response.wind.gust, // miles/hr
-		cloudiness: response.clouds.all / 100, // Transformed to percent
-		rainVolume: response.rain,
-		snowVolume: response.snow,
-		sunrise: luxon.DateTime.fromSeconds(response.sys.sunrise),
-		sunset: luxon.DateTime.fromSeconds(response.sys.sunset),
-		weather: response.weather,
-	};
-	return weather;
+	return await weatherAPI.getCurrentWeather(latitude, longitude);
 }
 
 async function fetchForecast(latitude, longitude) {}
